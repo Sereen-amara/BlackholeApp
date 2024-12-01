a21baf9b178b877d97cfea80c27bf33bce3d30fd
Sereen-amara <93250567+Sereen-amara@users.noreply.github.com>
2024-12-01 22:07:41 +0000

    Initial commit

---
+++ src/main/java/com/example/blackholeapp/BlackholeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackholeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackholeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackholeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blackholeapp/BlackholeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackholeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackholeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
